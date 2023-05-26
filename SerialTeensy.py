a66ffac982bbd9c006493b22f55622dbb0f3bd96
Steven Rodriguez <72773005+stevenrodriguezz@users.noreply.github.com>
2023-05-25 21:30:29 -0400

    Update SerialTeensy.py

--- SerialTeensy.py
+++ SerialTeensy.py
@@ -1,1 +1,27 @@
+import serial
+import time
+import os
+
+teensyDataFolder = "/home/stevenchurro/Projects/Testloops/teensyData/"
+teensyErrorFolder = "/home/stevenchurro/Projects/Testloops/teensyError/"
+
+# Replace the port and baudrate with the values used in your Teensy code
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+#os.listdir(teensyDataFolder)
+
+teensyDataFilename = teensyDataFolder + "teensyData-" + str(int(time.time())) + ".txt"
+teensyErrorFilename = teensyErrorFloder + "teensyError-" + str(int(time.time())) + ".txt"
+
+
+while True:
+    try:
+        if ser.in_waiting > 0:
+            data = ser.readline().decode()
+            with open(teensyDataFilename, 'a') as f:
+                f.write(str(time.time()) + "," + data)
+    except Exception as e:
+        with open(teensyErrorFilename, 'a') as f:
+            f.write(str(time.time()) + "," + str(e))
+    time.sleep(1)
 
