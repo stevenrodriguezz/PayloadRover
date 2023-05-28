2a506a77f09323ce4876fc852705bf53d8b018b6
Steven Rodriguez <72773005+stevenrodriguezz@users.noreply.github.com>
2023-05-28 18:29:18 -0400

    Update gpsLoop.py

--- gpsLoop.py
+++ gpsLoop.py
@@ -3,12 +3,11 @@ import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
+position = None  # Define position before the loop
+
 while True:
     if ser.in_waiting > 0:
         gpsRAW = ser.read_all().decode("utf-8")
-        with open("TransmitterData.txt", "a") as f:
-            f.write(gpsRAW + "\r\n")
-		#f.close()
         gpsSeparate = gpsRAW.split("\r\n")
         for i in gpsSeparate:
             if "GNGGA" in i:
@@ -20,6 +19,9 @@ while True:
                     "W": gpsParsed[5],
                     "altitude": gpsParsed[8],
                 }
-               #latitude = gpsData[2]
-               # longitude = gpsData[4]
+        if position is not None:  # Check if position is defined
+            with open("TransmitterData.txt", "a") as f:
+                f.write(str(position) + "\r\n")
+                position = None  # Reset position after writing to the file
+                
     time.sleep(2)
