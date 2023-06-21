3e064ba170c2ad211e8d06d10d7d4db885c2057d
Steven Rodriguez <72773005+stevenrodriguezz@users.noreply.github.com>
2023-06-21 11:03:09 -0700

    Update teensyLoop.ino

--- Teensy Arduino Code & Libraries /teensyLoop.ino
+++ Teensy Arduino Code & Libraries /teensyLoop.ino
@@ -8,18 +8,11 @@
 #define BNO055_SAMPLERATE_DELAY_MS 2000
 #define BME280_SAMPLERATE_DELAY_MS 1000
 
-//#define GPS_SAMPLERATE_DELAY_MS 1000
-
 void setup() {
-  // put your setup code here, to run once:
-  //Serial1.begin(9600);
-  //Serial1.setTimeout(3000);
   Serial.begin(9600);
-  //while (!Serial);
-  Serial.println("Beginning BME, ADXL Test: ");
+  Serial.println("Beginning BME, BNO Test: ");
 
   BME_START();
-  //BNO_START();
   SERVO_START();
 }
 
@@ -36,10 +29,5 @@ void loop() {
     BME_READ();
     BMETime = millis() + BME280_SAMPLERATE_DELAY_MS;
   }
-
-  /*if (millis() > GPSTime){
-    GPS_READ();
-    GPSTime = millis() + GPS_SAMPLERATE_DELAY_MS;
-  }*/
   
   SERVO_READ();
