4fa789d4117103216e7aee6dda2ad90fb8ed7038
Steven Rodriguez <72773005+stevenrodriguezz@users.noreply.github.com>
2023-06-21 11:10:54 -0700

    Update teensyLoop.ino

--- Teensy Arduino Code & Libraries /teensyLoop.ino
+++ Teensy Arduino Code & Libraries /teensyLoop.ino
@@ -1,18 +1,21 @@
 #include <Adafruit_Sensor.h>
 
+//Header files located in current github repository folder
 #include "BME.h"
 #include "BNO.h"
 #include "payload_servos.h"
 #include "gps.h"
 
+//Constants representing delay period between sensor readings in milliseconds.
 #define BNO055_SAMPLERATE_DELAY_MS 2000
 #define BME280_SAMPLERATE_DELAY_MS 1000
 
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Initiate serial communication with baud rate of 9600. 
   Serial.println("Beginning BME, BNO Test: ");
 
-  BME_START();
+  BME_START(); // The BME_START() function and SERVO_START() function are called from the BME.h and payload_servo.h header files respectively. 
   SERVO_START();
 }
 
