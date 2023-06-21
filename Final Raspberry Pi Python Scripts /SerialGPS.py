dc6580b48b4349519d1d32fb1f6fe7685dd9fee8
Steven Rodriguez <72773005+stevenrodriguezz@users.noreply.github.com>
2023-06-21 10:26:15 -0700

    Update SerialGPS.py

--- Final Raspberry Pi Python Scripts /SerialGPS.py
+++ Final Raspberry Pi Python Scripts /SerialGPS.py
@@ -2,19 +2,20 @@ import serial
 import time
 import os
 
+# Assign string value representing path to gpsData folder to a variable called GPSDataFolder 
 GPSDataFolder = "/home/seds/SEDS/SEDS-PayloadRover2023/gpsData/"
 
-ser = serial.Serial('/dev/ttyACM0', 9600) #try either ACM0 or ACM1 & plug into middle 2 usb ports of Pi 4
+# Initialize variable as a Serial object to allow communication with GPS via USB located at either '/dev/ttyACM0' or  '/dev/ttyACM1' 
+ser = serial.Serial('/dev/ttyACM0', 9600) 
 
+#This line creates a string representing the filename for the GPS data file where the GPS data will be appended to inside of the GPSDataFolder.
 gpsDataFilename = GPSDataFolder + "GPS-" + str(int(time.time())) + ".txt"
-
-#position = None  # Define position before the loop
 
 while True:
     if ser.in_waiting > 0:
         gpsRAW = ser.read_all().decode("utf-8")
         with open(gpsDataFilename, "a") as f:
                     f.write("Time:" + str(time.time()) + "\r\n" + gpsRAW)
-                    #position = None  # Reset position after writing to the file
+                    
     time.sleep(1)
 sleep(1)
