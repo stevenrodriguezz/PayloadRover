d1c8e50ee034708a3b39824e6fd5ae0df77c0563
Steven Rodriguez <72773005+stevenrodriguezz@users.noreply.github.com>
2023-06-25 00:17:47 -0400

    Update payload_servos.h

--- Teensy Arduino Code & Libraries /payload_servos.h
+++ Teensy Arduino Code & Libraries /payload_servos.h
@@ -9,12 +9,11 @@ Servo servo2;
 
 
 void SERVO_START() {
-servo1.attach(servoPin1);// put your setup code here, to run once:
+servo1.attach(servoPin1);
 servo2.attach(servoPin2);
 }
 
 void SERVO_READ() {
-  //Serial.println("Writing to Servos");
   servo1.writeMicroseconds(700);
   servo2.writeMicroseconds(2300);
 
